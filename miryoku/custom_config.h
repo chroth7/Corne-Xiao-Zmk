3d1bc901702f5ec12ec50eefb47247e35eacb1b2
Mikilio <official.mikilio@gmail.com>
2024-05-19 20:29:50 +0200

    vanilla config with gaming layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_CLIPBOARD_FUN
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+#define MIRYOKU_LAYER_GAME \
+&kp ESC,          &kp N1,            &kp N2,            &kp N3,            &kp N4,            U_NA,              &u_to_U_BASE,      U_NA,          U_NA,          U_NA, \
+&kp TAB,          &kp Q,             &kp W,             &kp E,             &kp R,             U_NA,              U_NA,              U_NA,          U_NA,          U_NA, \
+&kp LSHFT,        &kp A,             &kp S,             &kp D,             &kp F,             U_NA,              U_NA,              U_NA,          U_NA,          U_NA, \
+U_NP,              U_NP,             &kp LCTRL,         &kp LALT,          &kp SPACE,         &tog U_TAP,        U_NA,              U_NA,          U_NP,          U_NP
+ 
