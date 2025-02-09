cfe124f5b1507a5b9c7d90a5d22362836b3275f1
Christian Roth <christianroth@Christians-MacBook-Pro.local>
2025-02-09 21:31:53 +0100

    Try again.

--- miryoku/mapping/42/xiao_corne.h
+++ miryoku/mapping/42/xiao_corne.h
@@ -1,7 +1,7 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+#if !defined (MIRYOKU_LAYOUTMAPPING_XIAO_CORNE)
 
 #define XXX &none
 
@@ -19,7 +19,7 @@
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+XXX  K20  K21  K22  K23  K24       K27  K28  K29  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
@@ -38,4 +38,4 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_XIAO_CORNE
