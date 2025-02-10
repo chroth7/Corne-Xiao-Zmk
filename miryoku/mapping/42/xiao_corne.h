24d3c52eb2a3ca42f965922884eddfe8582cd93c
Christian Roth <christianroth@Mac.localdomain>
2025-02-10 11:39:07 +0100

    Try fix 4th row.

--- miryoku/mapping/42/xiao_corne.h
+++ miryoku/mapping/42/xiao_corne.h
@@ -15,12 +15,12 @@
      K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,           K27, K28, K29, K30, K31, \
-     K32, K33, K34, K35, K36,           K37, N38, N39, N25, N26\
+     K34, K35, K36,           K37, N38, N39, N25, N26, K32, K33 \
 ) \
 XXX  K00  K01  K02  K03  K04            K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14            K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  N25  N26  K27  K28  K29  K30  K31  XXX \
-               K32  K33  K34       K35  K36  K37
+               K32  K33  K34            K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
